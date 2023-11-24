21a0ec28608287eabf178ee8c3a67e45abe2055a
Lawi Kiprotich <fleeklawi@gmail.com>
2023-11-24 13:54:24 +0300

    Auth

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
+from .forms import UserSignupForm, CustomAuthenticationForm
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = UserSignupForm
+    form = CustomAuthenticationForm
+    model = CustomUser
+    list_display_links = ['email']
+    search_fields = ['email',]
+    ordering = ('email', )
+    list_display = ('email', )
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils import timezone
+from .constants import constants as user_constants
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('user_type', user_constants.SUPERUSER)        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('You must be logged in as a superuser!')
+        return self.create_user(email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
+    username = None
+    email = models.EmailField(unique=True, null=True, db_index=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    user_type = models.PositiveSmallIntegerField(choices=user_constants.USER_TYPE_CHOICES)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    objects = CustomUserManager()
